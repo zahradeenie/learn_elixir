33679b5f019c2ce41684c1acb8de752e871a73e2
Zahra <zahradeenie@gmail.com>
2020-09-04 22:16:07 +0200

    build a guessing game

---
+++ lib/guessing_game.ex
@@ -0,0 +1,35 @@
+defmodule GuessingGame do
+
+  def guess(low, high) do
+    answer = IO.gets("Hmm, maybe you're thinking of #{mid(low, high)}\n")
+
+    case String.trim(answer) do
+      "bigger" ->
+        bigger(low, high)
+
+      "smaller" ->
+        smaller(low, high)
+
+      "yes" ->
+        "I guessed it!"
+
+      _ ->
+        IO.puts(~s{"Type in 'bigger', 'smaller' or 'yes'"})
+        guess(low, high)
+    end
+  end
+
+  def mid(low, high) do
+    div(low + high, 2)  
+  end
+
+  def bigger(low, high) do
+    new_low = min(high, mid(low, high) + 1)
+    guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low, high) - 1)
+    guess(low, new_high)
+  end
+end
